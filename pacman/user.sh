20d9b04af3d59e41d32023e784917717d1189101
U <U>
2023-07-09 04:42:33 +0800

    update

--- pacman/user.sh
+++ pacman/user.sh
@@ -33,7 +33,6 @@ usermod -s "$shell" "$user"
 usermod -s "$shell" "$root"
 mkhomedir_helper "$user"
 mkhomedir_helper "$root"
-password='$6$9We4mW90FP3l9Gx7$dy4VdP0YQlAjT2o4wkKP515XhwTUzpOj/GB6wb7Zu2gnxJCBk1pVrb2JqQJLXk/YX9k.ZbV/21KEdI7JD6TPR0'
 usermod -p "$password" "$root"
 usermod -p "$password" "$user"
 
