557cf4a32f39679631a481676c2b2ae27f1aa974
u <u>
2023-08-16 14:52:00 +0300

    update

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -9,16 +9,22 @@ localectl set-locale en_US.UTF-8
 localectl set-keymap jp106
 
 ds(){
-    local f=/etc/xdg/autostart/$1.desktop
-    if [ -f "$f" ]; then
-        rm -v "$f.disabled"
-        mv -vT "$f"{,.disabled}
+    local fp="/etc/xdg/autostart/$1.desktop"
+    local ft="/etc/xdg/autostart-disabled/$1.desktop"
+    mkdir -pv /etc/xdg/autostart-disabled
+    if [ -f "$fp" ]; then
+        rm -v "$ft"
+        mv -v "$fp" "$ft"
     fi
 }
 
 ds xfsettingsd
 ds org.gnome.Evolution-alarm-notify
 ds ca.andyholmes.Valent-autostart
+ds geoclue-demo-agent
+ds org.kde.kdeconnect.daemon
+ds org.kde.kalendarac
+ds kaccess
 
 kver="$(ls /lib/modules)"
 
