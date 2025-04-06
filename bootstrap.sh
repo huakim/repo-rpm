f0da77023ac6051cb70da412e70d1f9019c8b5e4
u <u>
2025-04-06 10:48:49 +0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,7 +38,7 @@ i "${idir}" "${smp}"
 #echo "$dir"
 #chroot "$dir" /bin/bash
 #chroot "$dir" /bin/bash "/${idir}/pacman/aptat.sh"
-INSTALLROOT="${dir}" CACHEDIR="${smp}/pacman/var/cache/zypp" python3 "${smp}/pacman/apt-$1.py"
+INSTALLROOT="${dir}" CACHEDIR="${smp}/pacman/var/cache/libdnf5" python3 "${smp}/pacman/apt-$1.py"
 #i extra "${smp}"
 #chroot . /bin/bash
 #chroot . /bin/bash "/extra/pacman/apt-${1}.py"
@@ -47,9 +47,9 @@ chroot "${dir}" /usr/bin/env bash "/${idir}/pacman/copy.sh"
 chroot "${dir}" /usr/bin/env bash "/${idir}/pacman/setup.sh"
 chroot "${dir}" /usr/bin/env "DEFAULTUSER=${DEFAULTUSER}" bash "/${idir}/pacman/user.sh"
 
-if [ -n "${INSTALL_NEW_RECOMMENDS}" ]; then
-  chroot "${dir}" /usr/bin/env zypper -C "${smp}/pacman/var/cache/zypp" --auto-agree-with-licenses --non-interactive -vvv --gpg-auto-import-keys install-new-recommends
-fi
+#if [ -n "${INSTALL_NEW_RECOMMENDS}" ]; then
+#  chroot "${dir}" /usr/bin/env zypper -C "${smp}/pacman/var/cache/zypp" --auto-agree-with-licenses --non-interactive -vvv --gpg-auto-import-keys install-new-recommends
+#fi
 #chroot "${dir}" /usr/bin/env "DEFAULTUSER=${DEFAULTUSER}" bash "/${idir}/pacman/tricks.sh"
 #chroot . /sbin/runuser -u lenovo -c 'cd /extra/home/lenovo; ./txt.sh'
 #DRACUT_ARGS="${DRACUT_ARGS:---force-drivers usb_storage}"
